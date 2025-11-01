9349f81d8c66689ecf461cfb91c38b701631feb0
lehai17 <lenguyendaihai17@gmail.com>
2025-11-01 23:25:53 +0700

    Add PhoneNumber column to User (PostgreSQL migration)

---
+++ CleanMate.Api/Migrations/AppDbContextSqlServerMigrations/20251101162522_AddPhoneNumberToUser_Postgres.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanMate.Api.Migrations.AppDbContextSqlServerMigrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneNumberToUser_Postgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
